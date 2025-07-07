7f27219fd9b07eedd850387e2112ec682d353bb6
e.d.gerasin <e.d.gerasin@yandex.ru>
2025-07-07 13:20:21 +0300

    new Решил проблему с CORS

--- app/app.py
+++ app/app.py
@@ -16,10 +16,7 @@ ollama_client = ollama.Client(host='http://127.0.0.1:11434')
 # Настройки CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "chrome-extension://ingflainldpdigbnkbmlglinefminhpn",  # Замените на ID вашего расширения
-        "http://localhost",  # Для тестирования из браузера
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -38,11 +35,11 @@ async def generate_check_list(request: RequirementRequest):
 
     Параметры:
     - requirement: Текстовое описание требования
-    - model: Модель Ollama для использования (по умолчанию mistral:latest)
+    - model: Модель Ollama для использования (по умолчанию deepseek-r1:latest )
     """
     try:
         prompt = """
-        Напиши максимально подробный чек-лист проверок для следующего требования к функционалу ПО на русском языке:
+        Ты senior-тестировщик ПО. Напиши максимально подробный чек-лист проверок для следующего требования к функционалу ПО на русском языке:
         {}
         
         Формат вывода:
@@ -68,6 +65,43 @@ async def generate_check_list(request: RequirementRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 
+@app.post("/generate_test_case/", summary="Генерация тест-кейсов по требованию")
+async def generate_test_case(request: RequirementRequest):
+    """
+    Сгенерирует тест-кейсов для заданного требования к функционалу ПО.
+
+    Параметры:
+    - requirement: Текстовое описание требования
+    - model: Модель Ollama для использования (по умолчанию deepseek-r1:latest )
+    """
+    try:
+        prompt = """
+        Напиши максимально подробные тест-кейсы проверок для следующего требования к функционалу ПО на русском языке:
+        {}
+        
+        Формат вывода:
+        'номер тест-кейса': 'Название тест-кейса. Шаги. Ожидаемый результат' 
+
+        Пример:
+        '1', 'Поле отображается на странице. 1.Ввести данные. 2 Нажать кнопку сохранить. Ожидаемый результат: данные сохранены', '2', 'Поле отображается на странице неверно. 1.Ввести неверные данные. 2 Нажать кнопку сохранить. Ожидаемый результат: данные не сохранены',
+
+        """.format(request.requirement)
+
+        response = ollama_client.generate(
+            model=request.model,
+            prompt=prompt,
+            format='json',
+            stream=False
+        )
+
+        json_result = json.loads(response['response'].strip("'"))
+
+        return json_result
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
 @app.post("/test/", summary="Тестовый метод для предоставления ответа")
 async def for_test(request: RequirementRequest):
     """
@@ -75,7 +109,7 @@ async def for_test(request: RequirementRequest):
 
     Параметры:
     - requirement: Текстовое описание требования
-    - model: Модель Ollama для использования (по умолчанию mistral:latest)
+    - model: Модель Ollama для использования (по умолчанию deepseek-r1:latest )
     """
     json_result = json.loads(example_response)
     result = ''
